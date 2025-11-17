5949205bc21db328531269ed09c825562c2449d3
SayCV <sssxyc@163.com>
2025-11-17 11:34:25 +0800

    cmake/zig-toolchain.cmake - config CMAKE_ASM_COMPILER

--- cmake/zig-toolchain.cmake
+++ cmake/zig-toolchain.cmake
@@ -56,6 +56,7 @@ endif()
 # Work around a bug in clangd where 'c++' is reordered on the command line
 set(CMAKE_C_COMPILER "${CMAKE_CURRENT_LIST_DIR}/zig-cc${SCRIPT_SUFFIX}" -target ${ZIG_TARGET})
 set(CMAKE_CXX_COMPILER "${CMAKE_CURRENT_LIST_DIR}/zig-c++${SCRIPT_SUFFIX}" -target ${ZIG_TARGET})
+set(CMAKE_ASM_COMPILER "${CMAKE_CURRENT_LIST_DIR}/zig-cc${SCRIPT_SUFFIX}" -target ${ZIG_TARGET})
 
 # This is working (thanks to Simon for finding this trick)
 set(CMAKE_AR "${CMAKE_CURRENT_LIST_DIR}/zig-ar${SCRIPT_SUFFIX}")
